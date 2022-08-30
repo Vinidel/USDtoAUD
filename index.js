2c21faa4ade77a9cfc0f30c4eb6c54850f07d679
Vinicius Delascio Fortunato <vinicius.delascio@foxtel.com.au>
2022-08-30 18:58:21 +1000

    make command line work

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const ratesService = require('./services/rates-service');
+const ingestService = require('./services/ingester-service');
+
+const FgRed = '\x1b[31m';
+const FgGreen = '\x1b[32m';
+const BgBlack = '\x1b[40m';
 
 async function t() {
-  const r = await ratesService.getAllByDate('2022-08-01');
-  console.log(r);
-  r.data.documents.forEach((element) => {
-    console.log(element);
-  });
-  return r;
+  const supportedFiles = ['csv'];
+  const args = process.argv;
+  const filePath = args[2];
+  const outputFilePath = 'result.csv';
+
+  if (!filePath) {
+    console.error(`${FgRed}%s${BgBlack} %s`, 'Please provide a path to the file in the command line', 'node index file/path.csv');
+    // eslint-disable-next-line no-useless-return
+    return;
+  }
+
+  const fileExtension = filePath.substring(filePath.length - 3);
+  if (!supportedFiles.includes(fileExtension)) {
+    console.error(`${FgRed}%s${BgBlack}`, `Brow I only except ${supportedFiles.join(',')}`);
+    // eslint-disable-next-line no-useless-return
+    return;
+  }
+
+  try {
+    console.log(`${FgGreen}%s${BgBlack} %s`, 'Ingesting file', `${__dirname}/services/${filePath}`);
+    await ingestService.readAndParseEtoro(`${__dirname}/${filePath}`);
+    console.log(`${FgGreen}%s${BgBlack} %s`, 'File ingested you should get new file in folder', outputFilePath);
+  } catch (error) {
+    console.log(`${FgRed}%s${BgBlack}`, error);
+  }
 }
 
 t();
--- services/ingester-service.js
+++ services/ingester-service.js
@@ -1,15 +1,84 @@
+/* eslint-disable no-param-reassign */
 const csv = require('csvtojson');
+const dateFns = require('date-fns');
+const { parse } = require('json2csv');
+const fs = require('fs');
 const ratesService = require('./rates-service');
 
 const csvPath = `${__dirname}/AUDUSD.csv`;
+// const csvPath = `${__dirname}/closedpositions.csv`;
 
-csv().fromFile(csvPath)
+const DAYS_IN_A_YEAR = 365;
+const HEADER_NAMES = [
+  'Position ID',
+  'Action',
+  'Amount',
+  'Units',
+  'Open Date',
+  'Close Date',
+  'Leverage',
+  'Spread',
+  'Profit',
+  'Open Rate',
+  'Close Rate',
+  'Take profit rate',
+  'Stop lose rate',
+  'Rollover Fees and Dividends',
+  'Copied From',
+  'Type',
+  'ISIN',
+  'Notes',
+  'AUD',
+  'TYPE (SHORT/LONG)',
+  'ProfitInAud',
+]
+
+const extractDateFromString = (dateString = '01/12/1970 00:00:00') => dateString.substring(0, 10).split('/');
+
+const readAndParseRates = () => {
+  csv().fromFile(csvPath)
+    .on('data', async (d) => {
+      const jsonStr = d.toString('utf8');
+      const j = JSON.parse(jsonStr);
+      j.date = new Date(j.Date).toISOString();
+      j.aud = Number.parseFloat(j.AUD.replace('$', ''));
+      delete j.Date;
+      delete j.AUD;
+      await ratesService.createRates(j);
+    });
+};
+
+const r = [];
+const readAndParseEtoro = async (filePath) => csv().fromFile(filePath)
   .on('data', async (d) => {
     const jsonStr = d.toString('utf8');
     const j = JSON.parse(jsonStr);
-    j.date = new Date(j.Date).toISOString();
-    j.aud = Number.parseFloat(j.AUD.replace('$', ''));
-    delete j.Date;
-    delete j.AUD;
-    await ratesService.createRates(j);
-  });
+    r.push(j);
+  })
+  .on('end', async () => {
+    const requestPromises = r.map(async (j) => {
+      const [closeDD, closeMM, closeYYYY] = extractDateFromString(j['Close Date']);
+      const [openDD, openMM, openYYYY] = extractDateFromString(j['Open Date']);
+      const dayRate = await ratesService.getAllByDate(`${closeYYYY}-${closeMM}-${closeDD}`);
+      const rate = dayRate.data[0];
+
+      // Compare dates
+      const differenceInDays = dateFns.differenceInDays(
+        new Date(`${closeYYYY}-${closeMM}-${closeDD}`),
+        new Date(`${openYYYY}-${openMM}-${openDD}`),
+      );
+
+      j.ProfitInAud = (rate.aud * j.Profit).toFixed(2);
+      j['TYPE (SHORT/LONG)'] = differenceInDays >= DAYS_IN_A_YEAR ? 'LONG' : 'SHORT';
+      return j;
+    });
+    const v = await Promise.all(requestPromises);
+    parse(v, { fields: HEADER_NAMES });
+    fs.writeFileSync('result.csv', parse(v, { fields: HEADER_NAMES }));
+  })
+  .on('error', (error) => { throw error; });
+
+module.exports = {
+  readAndParseEtoro,
+  readAndParseRates,
+};
